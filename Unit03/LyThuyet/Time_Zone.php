01fae9114a38b23d146fa2b053fb24e8a0b70da2
tungdzai <tungnguyen0603202@gmai.com>
2022-10-24 00:23:02 +0700

    Zent

--- Unit03/LyThuyet/Time_Zone.php
+++ Unit03/LyThuyet/Time_Zone.php
@@ -1,12 +1,16 @@
 <?php
 //THời gian tại Việt Nam.
- date_default_timezone_set('Asia/Ho_Chi_Minh');
+date_default_timezone_set('Asia/Ho_Chi_Minh');
 
- // Danh Sách time zone.
-$timezone=DateTimeZone::listIdentifiers();
-foreach ( $timezone as $item) {
-    echo $item."<br/>";
+// Danh Sách time zone.
+$timezone = DateTimeZone::listIdentifiers();
+foreach ($timezone as $item) {
+    echo $item . "<br/>";
 }
 
 //Đinh dạng ngày giờ.
 echo date('d/m/y -H:i:s');
+function ischeck()
+{
+    return true;
+}
