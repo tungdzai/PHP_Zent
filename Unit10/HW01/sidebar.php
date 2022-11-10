e2e2b332b69a947b87d6d42de4ffe5813700a939
tungdai <tungnguyen0603202@gmail.com>
2022-11-10 11:28:53 +0700

    PHP

--- Unit10/HW01/sidebar.php
+++ Unit10/HW01/sidebar.php
@@ -2,47 +2,82 @@
     <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
         <div class="sb-sidenav-menu">
             <div class="nav">
-                <a class="nav-link" href="index.php">
-                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                    Dashboard
-                </a>
-                <a class="nav-link" href="./v-category-list.php">
-                    <div class="sb-nav-link-icon"><i class="fas fa-th"></i></div>
-                    Trang
-                </a>
-
-                <a class="nav-link" href="#">
-                    <div class="sb-nav-link-icon"><i class="fas fa-th"></i></div>
-                    Bài viết
-                </a>
-                <div class="accordion accordion-flush" id="accordionFlushExample">
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="flush-headingOne">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                                Accordion Item #1
-                            </button>
-                        </h2>
-                        <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body">
-                                <p style="color: #1a1e21">hihi</p>
-                                <code>accordion-flush</code>
-                            </div>
+                <div class="accordion">
+                    <section class="accordion-item">
+                        <h1></h1>
+                        <div class="accordion-item-content">
+                            <p>Content for section 1</p>
                         </div>
-                    </div>
+                    </section>
+                    <section class="accordion-item">
+                        <h1>Section 2</h1>
+                        <div class="accordion-item-content">
+                            <p>Content for section 2</p>
+                        </div>
+                    </section>
+                    <section  class="accordion-item accordion-item--default">
+                        <h1>Section 3</h1>
+                        <div class="accordion-item-content">
+                            <p>Content for section 3</p>
+                            <p>This is my default state.</p>
+                        </div>
+                    </section>
+                    <section class="accordion-item">
+                        <h1>Section 4</h1>
+                        <div class="accordion-item-content">
+                            <p>Content for section 4</p>
+                        </div>
+                    </section>
                 </div>
-                <a class="nav-link" href="./v-category-list.php">
-                    <div class="sb-nav-link-icon"><i class="fas fa-th"></i></div>
-                    Sản phẩm
-                </a>
-                <a class="nav-link" href="./v-category-list.php">
-                    <div class="sb-nav-link-icon"><i class="fas fa-th"></i></div>
-                    Bán hàng
-                </a>
-                <a class="nav-link" href="./v-category-list.php">
-                    <div class="sb-nav-link-icon"><i class="fas fa-th"></i></div>
-                    User
-                </a>
+
             </div>
         </div>
     </nav>
 </div>
+<style>
+    /* The Magic */
+    .accordion:hover .accordion-item:hover .accordion-item-content,
+    .accordion .accordion-item--default .accordion-item-content {
+        height: 6em;
+    }
+
+    .accordion-item-content,
+    .accordion:hover .accordion-item-content {
+        height: 0;
+        overflow: hidden;
+        transition: height .25s;
+    }
+    /* Aesthetic */
+
+    .accordion .accordion-item {
+        background-image: linear-gradient(90deg, #eee, #f5f5f5, #eee);
+        border-bottom: 1px solid #666;
+        padding: 1em;
+        color: #eee;
+    }
+
+    .accordion h1 {
+        margin: 0;
+    }
+
+    .accordion .accordion-item:nth-child(1) {
+        background-color: hsl(200, 80%, 60%);
+        background-image: linear-gradient(-90deg, hsl(200, 80%, 60%), hsl(200, 80%, 45%) 2em, hsl(200, 80%, 60%));
+
+    }
+
+    .accordion .accordion-item:nth-child(2) {
+        background-color: hsl(280, 60%, 45%);
+        background-image: linear-gradient(-90deg, hsl(280, 80%, 60%), hsl(280, 60%, 45%) 2em, hsl(280, 80%, 60%));
+    }
+
+    .accordion .accordion-item:nth-child(3) {
+        background-color: hsl(40, 80%, 40%);
+        background-image: linear-gradient(-90deg, hsl(40, 80%, 60%), hsl(40, 80%, 45%) 2em, hsl(40, 80%, 60%));
+    }
+
+    .accordion .accordion-item:nth-child(4) {
+        background-color: hsl(77, 37%, 35%);
+        background-image: linear-gradient(-90deg, hsl(80, 80%, 40%), hsl(80, 80%, 35%) 2em, hsl(80, 80%, 40%));
+    }
+</style>
