3edb7c7f87cfeb009ede7c1dda14b2ab730a1f4a
tungdai <tungnguyen0603202@gmail.com>
2022-11-17 13:29:13 +0700

    PHP

--- Unit10/HW01/index.php
+++ Unit10/HW01/index.php
@@ -22,7 +22,6 @@
         <main>
             <div class="container-fluid px-4">
                 <h1 class="mt-4">
-                <?= $_COOKIE["info"]?>
                 </h1>
             </div>
         </main>
